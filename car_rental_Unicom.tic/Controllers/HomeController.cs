65c11c02eca063275adce0d857c9686a81c75239
Mahendran kaajatharan <UT010025@unicomtic.com>
2025-09-06 12:40:53 +0530

    first commit

---
+++ car_rental_Unicom.tic/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using car_rental_Unicom.tic.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace car_rental_Unicom.tic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ car_rental_Unicom.tic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace car_rental_Unicom.tic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
